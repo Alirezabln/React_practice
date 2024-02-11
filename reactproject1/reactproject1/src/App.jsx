d5353ad7f252425dc84843275d67b04d6fb3bf6c
Alireza <alirezaboloorian@gmail.com>
2024-02-10 22:19:44 -0800

    Moved Game to top function

--- reactproject1/reactproject1/src/App.jsx
+++ reactproject1/reactproject1/src/App.jsx
@@ -11,9 +11,7 @@ function Square({ value, onSquareClick }) {
     );
 }
 
-export default function Board() {
-    const [xIsNext, setXIsNext] = useState(true);
-    const [squares, setSquares] = useState(Array(9).fill(null));
+function Board({ xIsNext, squares, onPlay }) {
     const winner = calculateWinner(squares)
     let status;
     if (winner) {
@@ -32,8 +30,7 @@ export default function Board() {
         } else {
             nextSquares[i] = 'O';
         }
-        setSquares(nextSquares);
-        setXIsNext(!xIsNext);
+        onPlay(nextSquares);
     }
 
     return (
@@ -77,5 +74,26 @@ export default function Board() {
         return null;
     }
 }
+
+export default function Game() {
+    const [xIsNext, setXIsNext] = useState(true);
+    const [history, setHistory] = useState([Array(9).fill(null)]);
+    const currentSquares = history[history.length - 1];
+
+    function handlePlay(nextSquares) {
+        setHistory([...history, nextSquares]);
+        setXIsNext(!xIsNext);
+    }
+    return (
+        <div className="game">
+            <div className="game-board">
+                <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+            </div>
+            <div className="game-info">
+                <ol>{/* TODO */}</ol>
+            </div>
+        </div>
+    );
+ }
 
 
